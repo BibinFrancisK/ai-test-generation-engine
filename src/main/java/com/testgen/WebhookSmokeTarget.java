2acea452d16740369afc57b1acd0d7c24b393184
Francis, Bibin <bibinfrancis404@outlook.com>
2026-07-16 20:32:16 +0530

    test: shrink target class back to a single method to fit LLM token budget

--- src/main/java/com/testgen/WebhookSmokeTarget.java
+++ src/main/java/com/testgen/WebhookSmokeTarget.java
@@ -5,24 +5,4 @@ public class WebhookSmokeTarget {
     public String greet(String name) {
         return "Hello, " + name + "!";
     }
-
-    public String farewell(String name) {
-        return "Goodbye, " + name + "!";
-    }
-
-    public String shout(String name) {
-        return name.toUpperCase() + "!!!";
-    }
-
-    public String whisper(String name) {
-        return name.toLowerCase() + "...";
-    }
-
-    public String repeat(String name, int times) {
-        return name.repeat(Math.max(times, 0));
-    }
-
-    public String reverse(String name) {
-        return new StringBuilder(name).reverse().toString();
-    }
 }
